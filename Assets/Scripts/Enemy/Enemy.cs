932630414436ed0a87409f989eec3f4969bb0abc
thomasmchugh03 <58193958+thomasmchugh03@users.noreply.github.com>
2020-05-09 16:38:48 -0400

    Add First Version

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 100;
+
+    public float speed;
+    public float patrollingDistance;
+    public float retreatingDistance;
+    public float waitTime;
+    public Transform moveSpot;
+
+    private GameObject player;
+    private float timeLeft;
+    
+
+    private Vector2 moving;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        moveSpot = Instantiate(moveSpot);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        timeLeft = waitTime;
+        moveSpot.position = new Vector2(Random.Range(-3, 3), Random.Range(-3, 3));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Movement();
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if(health - damage < 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            health -= damage;
+        }
+    }
+
+    private void Movement()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) < retreatingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, -speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.transform.position) > patrollingDistance)
+        {
+            Patrol();
+        }
+
+    }
+
+    private void Patrol() 
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        
+        if(timeLeft <= 0)
+        {
+            moveSpot.position = new Vector2(Random.Range(-3, 3), Random.Range(-3, 3));
+            timeLeft = waitTime;
+        }
+        else
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        
+    }
+
+
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private float move;
+
+    public Rigidbody2D mainbody;
+    public Animator animator;
+    public GameObject dashEffect;
+
+    Vector2 movement;
+
+    void Start()
+    {
+        mainbody = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
+        move = moveSpeed;
+    }
+
+    void Update()
+    {
+        //Dashing is called as a coroutine because of inconsistency with Time.deltaTime making dashtime never get assigned consistently to startDashTime
+        if (Input.GetKeyDown(KeyCode.Space) && animator.GetFloat("Speed") > 0)
+        {
+            StartCoroutine("Dashing");
+        }
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        //Time.fixedDeltaTime is there because it results in consistent movement speed in game, as to how I am uncertain
+        mainbody.MovePosition(mainbody.position + movement * move * Time.fixedDeltaTime);
+
+    }
+
+    IEnumerator Dashing()
+    {
+
+        //Creat dash animation, then destroy it to not take up resources
+        GameObject cloud = Instantiate(dashEffect, transform.position, Quaternion.identity);
+
+        //Assign speed to dashspeed, wait the alloted time, then return to normal
+        move = dashSpeed;
+        yield return new WaitForSeconds(dashTime);
+        move = moveSpeed;
+        Destroy(cloud, .5f);
+
+    }
+}
